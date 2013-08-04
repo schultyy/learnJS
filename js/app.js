60b2d2fd95ce9f50ecbfc544306b3cba32e990db
schultyy <janschulte90@googlemail.com>
2013-08-05 00:22:29 +0200

    renamed to exercises.js. can show exercises in tab control

--- js/app.js
+++ js/app.js
@@ -3,8 +3,29 @@ $(document).ready(function(){
     $('#about').hide('slow');
     $('#tutorial').show('fast');
 
-    $.getJSON('exercises.json', function(data){
-      alert("boo");
+    $.getJSON('exercises.js', function(data){
+      $.each(data, createExercise);
     });
   });
 });
+
+function createExercise(index, currentExercise){
+
+  var elementHeader = $("<li>")
+    .html($("<a>").attr("data-toggle","tab")
+                  .attr("href", "#" + currentExercise.key)
+                  .html(currentExercise.title));
+  if(index == 0)
+    elementHeader.addClass("active");
+
+  $("#tab-header").append(elementHeader);
+  var elementBody = $("<div>")
+        .addClass("tab-pane")
+        .attr("id", currentExercise.key)
+        .html(currentExercise.description);
+
+  if(index == 0)
+    elementBody.addClass("active");
+
+  $(".tab-content").append(elementBody);
+}
