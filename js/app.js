971a8351bc8f02ee2b077713bf78c011e4b29491
schultyy <janschulte90@googlemail.com>
2013-08-05 00:00:59 +0200

    corrected invalid json

--- js/app.js
+++ js/app.js
@@ -2,5 +2,9 @@ $(document).ready(function(){
   $('#start-tutorial').click(function(){
     $('#about').hide('slow');
     $('#tutorial').show('fast');
+
+    $.getJSON('exercises.json', function(data){
+      alert("boo");
+    });
   });
 });
