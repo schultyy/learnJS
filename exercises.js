eeeead7416c5b4fcc984a00de015a8639751e7de
schultyy <janschulte90@googlemail.com>
2013-08-06 06:15:51 +0200

    added style for code samples.

--- exercises.js
+++ exercises.js
@@ -12,5 +12,5 @@
 {
   "key" : "exc_3",
   "title" : "Storing functions",
-  "description" : "We want to store a collection of functions for callback management. The most challenging thing is, how can you be sure, that no function was added twice? The solution is, that every callback gets a unique id assigned.<br />Tasks:<ul><li>Create an object called 'store'</li><li>'store' has a function called 'add' which accepts a callback as argument</li><li>'store' has a property called nextId which holds the next free id</li><li>Save all functions in an array called 'cache'</li></ul>"
+  "description" : "We want to store a collection of functions for callback management. The most challenging thing is, how can you be sure, that no function was added twice? The solution is, that every callback gets a unique id assigned.<br />Tasks:<ul><li>Create an object called <span class='code'>store</span></li><li><span class='code'>store</span> has a function called <span class='code'>add</span> which accepts a callback as argument</li><li><span class='code'>store</span> has a property called nextId which holds the next free id</li><li>Save all functions in an array called <span class='code'>cache</span></li><li>When a new callback is added, check if it has already an id assigned. If yes, discard. If no, assigned the next free id and add it to the cache.</li></ul>"
 }]
