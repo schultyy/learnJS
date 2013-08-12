fc6f5a7c488cedc8be8227d526518e6fcd87e44c
schultyy <janschulte90@googlemail.com>
2013-08-12 06:08:33 +0200

    EXC_3 Check if store has cache property

--- js/testcases.js
+++ js/testcases.js
@@ -8,7 +8,10 @@ var testCatalog = {
     try{
       results.push(assert(typeof store !== 'undefined', "store is defined"));
       results.push(assert(store.hasOwnProperty("nextId"), "Store has a next nextId property"));
-      results.push(assert(store.hasOwnProperty("add") && typeof store.add == "function", "Store has add method"))
+      results.push(assert(store.hasOwnProperty("add") && typeof store.add == "function", "Store has add method"));
+      results.push(assert(store.hasOwnProperty("cache"), "Store has cache property"));
+
+      
     }
     catch(error){
       
